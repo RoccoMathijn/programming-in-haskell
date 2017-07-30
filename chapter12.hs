5c180f0b3427955effe4af6d698ae68066154505
rmathijn <rocco.mathijnandela@dhl.com>
2017-07-30 22:10:40 +0100

    add solution of q4 of chapter12

--- chapter12.hs
+++ chapter12.hs
@@ -4,26 +4,43 @@ module Chapter12 where
 data Tree a = Leaf | Node (Tree a) a (Tree a)
               deriving Show
 
-instance Functor Tree where
-  -- fmap (a -> b) -> Tree a -> Tree b
-  fmap g (Leaf x)     -> Leaf (g x)
-  fmap g (Node l x r) -> Node (fmap g l) (g x) (fmap g r)
+--instance Functor Tree where
+--  -- fmap (a -> b) -> Tree a -> Tree b
+--  fmap g (Leaf x)     = Leaf (g x)
+--  fmap g (Node l x r) = Node (fmap g l) (g x) (fmap g r)
 
 -- 2.
-instance Functor ((->) a) where
-  -- fmap (b -> c) -> (a -> b) -> (a -> c)
-  fmap = (.)
+--instance Functor ((->) a) where
+--  -- fmap (b -> c) -> (a -> b) -> (a -> c)
+--  fmap = (.)
 
 -- 3.
-instance Applicative ((->) p) where
-  -- pure :: a -> f a
-  -- pure :: a -> (->) p a
-  -- pure :: a -> (p -> a)
-  -- pure :: a -> p -> a
-  pure = const
+--instance Applicative ((->) p) where
+--  -- pure :: a -> f a
+--  -- pure :: a -> (->) p a
+--  -- pure :: a -> (p -> a)
+--  -- pure :: a -> p -> a
+--  pure = const
   
-  -- (<*>) :: f (a -> b) -> f a -> f b
-  -- (<*>) :: ((->) p (a -> b)) -> ((->) p a) -> ((->) p b) 
-  -- (<*>) :: (p -> (a -> b)) -> (p -> a) -> (p -> b) 
-  -- (<*>) :: (p -> a -> b) -> (p -> a) -> (p -> b)
-  g (<*>) h = \x -> g x (h x)
+--  -- (<*>) :: f (a -> b) -> f a -> f b
+--  -- (<*>) :: ((->) p (a -> b)) -> ((->) p a) -> ((->) p b) 
+--  -- (<*>) :: (p -> (a -> b)) -> (p -> a) -> (p -> b) 
+--  -- (<*>) :: (p -> a -> b) -> (p -> a) -> (p -> b)
+--  g <*> h = \x -> g x (h x)
+
+-- 4.
+newtype ZipList a = Z [a]
+                    deriving Show
+
+instance Functor ZipList where
+  -- fmap (a -> b) -> ZipList a -> ZipList b
+  fmap g (Z xs) =  Z (fmap g xs)
+
+instance Applicative ZipList where
+  -- pure :: a -> f a
+  -- pure :: a -> ZipList a
+  pure x = Z (repeat x)
+
+  -- <*> :: f (a -> b) -> f a -> f b
+  -- <*> :: ZipList (a -> b) -> ZipList a -> ZipList b
+  (Z gs) <*> (Z hs) = Z [g h | (g, h) <- zip gs hs] 
