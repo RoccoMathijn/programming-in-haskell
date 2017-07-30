61a5d6b25aa8969d232cf2a30ed0e855bf2f0c79
rmathijn <rocco.mathijnandela@dhl.com>
2017-07-30 12:35:57 +0100

    add solution of q8 chapter08

--- chapter08.hs
+++ chapter08.hs
@@ -52,8 +52,8 @@ folde f g e = case e of
               Add x y -> g (folde f g x) (folde f g y)
 
 -- 6.
-eval :: Expr -> Int
-eval e = folde (\x -> x) (\x y -> x + y) e
+eval' :: Expr -> Int
+eval' e = folde (\x -> x) (\x y -> x + y) e
 
 -- 7.
 --instance Eq a => Eq (Maybe a) where
@@ -66,4 +66,50 @@ eval e = folde (\x -> x) (\x y -> x + y) e
 --  (x:xs) == (y:ys) = x == y && xs == ys
 --  _ == _ = False
 
+-- 8.
 
+data Prop = Const Bool
+          | Var Char
+          | Not Prop
+          | And Prop Prop
+          | Imply Prop Prop
+          | Disj Prop Prop
+          | Eq Prop Prop
+
+type Assoc k v = [(k,v)]
+type Subst = Assoc Char Bool
+
+find :: Eq k => k -> Assoc k v -> v
+find k t = head [v | (k', v) <- t, k == k']
+
+eval :: Subst -> Prop -> Bool
+eval _ (Const b)    = b
+eval s (Var x)      = find x s
+eval s (Not p)      = not (eval s p)
+eval s (And p q)    = eval s p && eval s q
+eval s (Imply p q)  = eval s p <= eval s q
+eval s (Disj p q)   = not (eval s p) && not (eval s q)
+eval s (Eq p q)     = eval s p == eval s q
+
+vars :: Prop -> [Char]
+vars (Const _)   = []
+vars (Var x)     = [x]
+vars (Not p)     = vars p
+vars (And p q)   = vars p ++ vars q
+vars (Imply p q) = vars p ++ vars q
+
+bools :: Int -> [[Bool]]
+bools 0 = [[]]
+bools n = map (False:) bss ++ map (True:) bss
+          where bss = bools (n-1)
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) = x : filter (/= x) (rmdups xs)
+
+subst :: Prop -> [Subst]
+subst p = map (zip vs) (bools (length vs))
+          where vs = rmdups (vars p)
+
+isTaut :: Prop -> Bool
+isTaut p = and [eval s p | s <- subst p]
