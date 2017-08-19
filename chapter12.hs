8d65b7199bbdc94047743049ff090031ac2a3ed3
rmathijn <rocco.mathijnandela@dhl.com>
2017-08-19 13:57:05 +0200

    Fix bug in CH12 Q1

--- chapter12.hs
+++ chapter12.hs
@@ -6,7 +6,7 @@ data Tree a = Leaf | Node (Tree a) a (Tree a)
 
 --instance Functor Tree where
 --  -- fmap (a -> b) -> Tree a -> Tree b
---  fmap g (Leaf x)     = Leaf (g x)
+--  fmap g Leaf         = Leaf
 --  fmap g (Node l x r) = Node (fmap g l) (g x) (fmap g r)
 
 -- 2.
